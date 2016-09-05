e8e24e0c1f3c2aa50c742060c3e2d575042fbbc4
Minlison <yuanhang.1991@icloud.com>
2016-09-05 20:25:22 +0800

    增加部分注释，删除部分无用功能代码

--- objc_change_prefix.sh
+++ objc_change_prefix.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-# 源文件 https://github.com/fsamuel-bs/objc_prefix_changer.git
+
 #CLOSE XCODE BEFORE RUNNING THE SCRIPT. IT ALSO HAS GIT PRIVELEGES AND MESSES UP 
 # GIT MV EXECUTED IN THIS PROJECT
 
-#set -e
-
-all_extensions=".m .h .xib"
-prefix_from=`# PUT OLD PREFIX HERE`
-prefix_to=`# PUT NEW PREFIX HERE`
-project_name=`# PUT PROJECT NAME HERE`
-test_name=`# PUT TEST PROJECT NAME HERE`
+# set -e
+# 都更改哪些后缀类型的文件
+all_extensions=".m .h .xib .storyboard"
+prefix_from="旧的项目前缀"
+prefix_to="新的项目前缀"
+project_name="项目名"
+test_name="项目单元测试名"
 
 # 如果提示project文件找不到，请把下面的project, storyboard 路径换成绝对路径
 project=`find ${project_name}.xcodeproj -name "project.pbxproj"`
 storyboard=`find . -name "MainStoryboard.storyboard"`
 
-#replace_filename_in_file $file_name $new_file_name $file_to_replace_pattern
+# replace_filename_in_file $file_name $new_file_name $file_to_replace_pattern
+# 更改文件内部的类前缀
 replace_filename_in_file () {
   sed -e "s/\([^a-zA-Z]\)$1\([^a-zA-Z]\)/\1$2\2/g" \
       -e "s/^$1\([^a-zA-Z]\)/$2\1/g" \
@@ -24,7 +25,9 @@ replace_filename_in_file () {
 }
 export -f replace_filename_in_file
 
-#replace_filename_in_all_files $file_name $new_file_name
+# replace_filename_in_all_files $file_name $new_file_name
+# 更改当前类在所有相关文件中的命名
+# 例如当前类是AAAObject,在很多.h.m.xib中都有引用这个类，需要把所有引用这个类的文件都要更改一遍
 replace_filename_in_all_files() {
   files_with_m=()
   files_with_m=`find $project_name -type f -name "*.m" -exec grep -l [^a-zA-Z]$1[^a-zA-Z] {} ";"`
@@ -37,6 +40,10 @@ replace_filename_in_all_files() {
   files_with_xib=()
   files_with_xib=`find $project_name -type f -name "*.xib" -exec grep -l [^a-zA-Z]$1[^a-zA-Z] {} ";"`
   files_with_xib=(${files_with_xib[@]} `find $test_name -type f -name "*.xib" -exec grep -l [^a-zA-Z]$1[^a-zA-Z] {} ";"`)
+
+  files_with_storyboard=()
+  files_with_storyboard=`find $project_name -type f -name "*.storyboard" -exec grep -l [^a-zA-Z]$1[^a-zA-Z] {} ";"`
+  files_with_storyboard=(${files_with_storyboard[@]} `find $test_name -type f -name "*.storyboard" -exec grep -l [^a-zA-Z]$1[^a-zA-Z] {} ";"`)
 
   stack=()
 
@@ -52,11 +59,15 @@ replace_filename_in_all_files() {
     replace_filename_in_file $1 $2 $file_xib
   done )& stack[2]=$!
 
+  ( for files_storyboard in ${files_with_storyboard[@]}; do
+    replace_filename_in_file $1 $2 $files_storyboard
+  done )& stack[3]=$!
+
   ( replace_filename_in_file $1 $2 $project
-  )& stack[3]=$!
+  )& stack[4]=$!
 
   ( replace_filename_in_file $1 $2 $storyboard
-  )& stack[4]=$!
+  )& stack[5]=$!
 
   for job in ${stack[@]}; do
     wait $job
@@ -64,7 +75,8 @@ replace_filename_in_all_files() {
 }
 export -f replace_filename_in_all_files
 
-#replace_path $file_name $new_file_name $extension
+# replace_path $file_name $new_file_name $extension
+# 更改本地文件名
 replace_path () {
   all_old_paths=()
   all_old_paths=`find $project_name -type f -name "$1*$3"`
@@ -74,13 +86,20 @@ replace_path () {
     new_path=`echo $old_path | sed "s/$1/$2/g"`
     echo "Old path: $old_path"
     echo "New path: $new_path"
-    git mv $old_path $new_path
+    # 本地git
+    # git mv $old_path $new_path
+    echo =====rename_filename start========
+    echo ===$old_path===$new_path======
+    # 重命名文件
+    mv $old_path $new_path
+    echo =====rename_file end========
   done
 }
 export -f replace_path
 
 
 main () {
+  # 用来记录文件是否更新过
   files_already_updated=()
   for extension in $all_extensions
   do
@@ -96,40 +115,31 @@ main () {
         file_name_without_prefix="${file_name:${#prefix_from}}"
         new_file_name="$prefix_to$file_name_without_prefix"
       else
-        new_file_name="$prefix_to$file_name"
+        # 是否要给没有前缀的文件增加前缀
+        # new_file_name="$prefix_to$file_name"
+        new_file_name="$file_name"
+        continue
       fi
-
+      echo '---------------------------------------------'
       echo "Current file name: $file_name"
       echo "New file name: $new_file_name"
-
-      replace_path $file_name $new_file_name $extension
-
-      echo '---------------------------------------------'
-
       echo "Rename file reference with name ending in *$extension"
 
-      #${files_already_updated[*]/%$file_name/}: for-each on files_already_updated executing sed s/$file_name$//
-      if [ "${files_already_updated[*]/%$file_name/}" == "${files_already_updated[*]}" ]
+      # 记录更新过的文件
+      file_update_name="$file_name$extension"
+
+      if [ "${files_already_updated[*]/%$file_update_name/}" == "${files_already_updated[*]}" ]
       then
         replace_filename_in_all_files $file_name $new_file_name
-        files_already_updated+=($file_name)
+        replace_path $file_name $new_file_name $extension
+        files_already_updated+=($file_update_name)
       fi
     done
   done
 
-  #Corrects possible changes in main.m
-  replace_path "${prefix_to}main" "main" ".m"
-  replace_filename_in_all_files "${prefix_to}main" "main"
-
-  #Sets standard header to new one
+  # Sets standard header to new one
+  # 设置新的类前缀配置文件
   sed -i "" -l "s/\(CLASSPREFIX = \)${prefix_from}/\1${prefix_to}/g" $project
 }
 
 main
-
-# 如果没有本地分支，下面的可以注释掉
-# 
-# git cm -m 'Renames all the files to new class prefix'
-# git add .
-# git reset HEAD refactor.sh
-# git cm -m 'Modifies prefix of classes inside all files to new class prefix'
